4ffa405bdb4d97364c097a3f613d7005eb23d1a3
Silver-Ink <martin.74@orange.fr>
2025-01-24 09:43:29 -0500

    track generates sections

--- track.gd
+++ track.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-var SC_section = preload("res://section.tscn")
+@export var length := 3
 
+var SC_section = preload("res://section.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	instanciate_sections()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +16,30 @@ func _process(delta: float) -> void:
 
 
 func instanciate_sections():
-	var section : Section = SC_section.instantiate()
+	var example_section : Section = SC_section.instantiate()
+	var shape : CollisionShape2D = example_section.get_node("Area2D/CollisionShape2D")
+	var rect : RectangleShape2D
+	var width : float
 	
+	var origin : Marker2D = get_node("Marker2D")
+	if (!is_instance_valid(origin)):
+		printerr("erreur, pas de marker2d en enfant de la track !")
 	
-	add_child(section)
+	if (!is_instance_valid(shape)):
+		printerr("erreur lecture de la node collisionshape")
+		
+	rect = shape.shape
+	if (!is_instance_valid(rect)):
+		printerr("erreur accès de la RectangleShape")
+	width = rect.size.x
+	
+	for i in range(length):
+		var section : Section = SC_section.instantiate()
+		add_child(section)
+		
+		section.position = origin.position
+		
+		section.position.x += i * width + width / 2.
+		
+	example_section.queue_free()
+	

e2359cce5a17c2614306961aa716f6754a55c12b
Silver-Ink <martin.74@orange.fr>
2025-01-24 14:36:00 -0500

    fix merge

--- note_sprite.gd
+++ note_sprite.gd
@@ -33,7 +33,7 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if (ligne == -1): return
+	#if (ligne == -1): return
 	var collider = area.get_parent()
 	if (collider is PlayingArea):
 		NotePlayer.play_note(height, type)
